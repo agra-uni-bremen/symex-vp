cd6e6df321166f412d5c5c8e86bc4c46d7d8c41e
Sören Tempel <tempel@uni-bremen.de>
2020-08-26 03:48:43 +0200

    symex-vp: separate dram and flash

--- vp/src/platform/symex/symex_main.cpp
+++ vp/src/platform/symex/symex_main.cpp
@@ -31,10 +31,14 @@ struct SymexOptions : public Options {
 public:
 	typedef unsigned int addr_t;
 
-	addr_t mem_size = 1024 * 1024 * 32;  // 32 MB ram, to place it before the CLINT and run the base examples (assume
-	                                     // memory start at zero) without modifications
-	addr_t mem_start_addr = 0x00000000;
-	addr_t mem_end_addr = mem_start_addr + mem_size - 1;
+	addr_t dram_size = 1024 * 16;  // 16 KB dram
+	addr_t dram_start_addr = 0x80000000;
+	addr_t dram_end_addr = dram_start_addr + dram_size - 1;
+
+	addr_t flash_size = 1024 * 1024 * 512;  // 512 MB flash
+	addr_t flash_start_addr = 0x00000000;
+	addr_t flash_end_addr = flash_start_addr + flash_size - 1;
+
 	addr_t clint_start_addr = 0x02000000;
 	addr_t clint_end_addr = 0x0200ffff;
 	addr_t sys_start_addr = 0x02010000;
@@ -94,15 +98,17 @@ int sc_main(int argc, char **argv) {
 	ISS core(*sim_solver, *sim_ctx, *sim_tracer, 0);
 	MMU mmu(core);
 	CombinedMemoryInterface core_mem_if("MemoryInterface0", core, &mmu);
-	SimpleMemory mem("SimpleMemory", opt.mem_size);
+	SimpleMemory dram("DRAM", opt.dram_size);
+	SimpleMemory flash("Flash", opt.flash_size);
 	ELFLoader loader(opt.input_program.c_str());
-	SimpleBus<2, 3> bus("SimpleBus");
+	SimpleBus<2, 4> bus("SimpleBus");
 	SyscallHandler sys("SyscallHandler");
 	CLINT<1> clint("CLINT");
 	DebugMemoryInterface dbg_if("DebugMemoryInterface");
 
-	MemoryDMI dmi = MemoryDMI::create_start_size_mapping(mem.data, opt.mem_start_addr, mem.size);
-	InstrMemoryProxy instr_mem(dmi, core);
+	MemoryDMI dram_dmi = MemoryDMI::create_start_size_mapping(dram.data, opt.dram_start_addr, dram.size);
+	MemoryDMI flash_dmi = MemoryDMI::create_start_size_mapping(flash.data, opt.flash_start_addr, flash.size);
+	InstrMemoryProxy instr_mem(flash_dmi, core);
 
 	std::shared_ptr<BusLock> bus_lock = std::make_shared<BusLock>();
 	core_mem_if.bus_lock = bus_lock;
@@ -112,28 +118,30 @@ int sc_main(int argc, char **argv) {
 	if (opt.use_instr_dmi)
 		instr_mem_if = &instr_mem;
 	if (opt.use_data_dmi) {
-		core_mem_if.dmi_ranges.emplace_back(dmi);
+		core_mem_if.dmi_ranges.emplace_back(dram_dmi);
 	}
 
-	loader.load_executable_image(mem.data, mem.size, opt.mem_start_addr);
-	core.init(instr_mem_if, data_mem_if, &clint, loader.get_entrypoint(), rv32_align_address(opt.mem_end_addr));
-	sys.init(mem.data, opt.mem_start_addr, loader.get_heap_addr());
+	loader.load_executable_image(flash.data, flash.size, opt.flash_start_addr);
+	core.init(instr_mem_if, data_mem_if, &clint, loader.get_entrypoint(), rv32_align_address(opt.dram_end_addr));
+	sys.init(dram.data, opt.dram_start_addr, loader.get_heap_addr());
 	sys.register_core(&core);
 
 	if (opt.intercept_syscalls)
 		core.sys = &sys;
 
 	// setup port mapping
-	bus.ports[0] = new PortMapping(opt.mem_start_addr, opt.mem_end_addr);
-	bus.ports[1] = new PortMapping(opt.clint_start_addr, opt.clint_end_addr);
-	bus.ports[2] = new PortMapping(opt.sys_start_addr, opt.sys_end_addr);
+	bus.ports[0] = new PortMapping(opt.flash_start_addr, opt.flash_end_addr);
+	bus.ports[1] = new PortMapping(opt.dram_start_addr, opt.dram_end_addr);
+	bus.ports[2] = new PortMapping(opt.clint_start_addr, opt.clint_end_addr);
+	bus.ports[3] = new PortMapping(opt.sys_start_addr, opt.sys_end_addr);
 
 	// connect TLM sockets
 	core_mem_if.isock.bind(bus.tsocks[0]);
 	dbg_if.isock.bind(bus.tsocks[1]);
-	bus.isocks[0].bind(mem.tsock);
-	bus.isocks[1].bind(clint.tsock);
-	bus.isocks[2].bind(sys.tsock);
+	bus.isocks[0].bind(flash.tsock);
+	bus.isocks[1].bind(dram.tsock);
+	bus.isocks[2].bind(clint.tsock);
+	bus.isocks[3].bind(sys.tsock);
 
 	// connect interrupt signals/communication
 	clint.target_harts[0] = &core;
