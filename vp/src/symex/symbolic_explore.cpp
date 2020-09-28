fc6218178aacc038f46e34d50ecefeda185410e4
Sören Tempel <tempel@uni-bremen.de>
2020-09-28 20:54:40 +0200

    symex: Allow execution of specific test cases

--- vp/src/symex/symbolic_explore.cpp
+++ vp/src/symex/symbolic_explore.cpp
@@ -13,14 +13,16 @@
 #include "symbolic_explore.h"
 #include "symbolic_context.h"
 
-static std::filesystem::path testcase_path;
+#define TESTCASE_ENV "SYMEX_TESTCASE"
+
+static std::filesystem::path *testcase_path = nullptr;
 static size_t errors_found = 0;
 
 static void
 report_handler(const sc_core::sc_report& report, const sc_core::sc_actions& actions)
 {
 	auto mtype = report.get_msg_type();
-	if (strcmp(mtype, "/AGRA/riscv-vp/host-error")) {
+	if (strcmp(mtype, "/AGRA/riscv-vp/host-error") || !testcase_path) {
 		sc_core::sc_report_handler::default_handler(report, actions);
 		return;
 	}
@@ -28,7 +30,7 @@ report_handler(const sc_core::sc_report& report, const sc_core::sc_actions& acti
 	clover::ExecutionContext &ctx = symbolic_context.ctx;
 	clover::ConcreteStore store = ctx.getPrevStore();
 
-	auto path = testcase_path / ("error" + std::to_string(++errors_found));
+	auto path = *testcase_path / ("error" + std::to_string(++errors_found));
 	std::ofstream file(path);
 	if (!file.is_open())
 		throw std::runtime_error("failed to open " + path.string());
@@ -44,25 +46,43 @@ create_testdir(void)
 
 	if (!(dirpath = mkdtemp(tmpl)))
 		throw std::system_error(errno, std::generic_category());
-	testcase_path = std::filesystem::path(dirpath);
+	testcase_path = new std::filesystem::path(dirpath);
+}
+
+static int
+run_test(const char *path, int argc, char **argv)
+{
+	std::string fp(path);
+	std::ifstream file(fp);
+	if (!file.is_open())
+		throw std::runtime_error("failed to open " + fp);
+
+	clover::ExecutionContext &ctx = symbolic_context.ctx;
+	clover::ConcreteStore store = clover::TestCase::fromFile(fp, file);
+
+	ctx.setupNewValues(store);
+	return sc_core::sc_elab_and_sim(argc, argv);
 }
 
 int
 symbolic_explore(int argc, char **argv)
 {
-	create_testdir();
-
 	clover::ExecutionContext &ctx = symbolic_context.ctx;
 	clover::Trace &tracer = symbolic_context.trace;
 
 	// Hide SystemC copyright message
 	setenv("SYSTEMC_DISABLE_COPYRIGHT_MESSAGE", "1", 0);
 
-	// Set report handler for detecting errors
-	sc_core::sc_report_handler::set_handler(report_handler);
-
 	// Use current time as seed for random generator
 	std::srand(std::time(nullptr));
+
+	char *testcase = getenv(TESTCASE_ENV);
+	if (testcase)
+		return run_test(testcase, argc, argv);
+	create_testdir();
+
+	// Set report handler for detecting errors
+	sc_core::sc_report_handler::set_handler(report_handler);
 
 	size_t paths_found = 0;
 	do {
@@ -88,7 +108,7 @@ symbolic_explore(int argc, char **argv)
 
 	if (errors_found > 0) {
 		std::cout << "Errors found: " << errors_found << std::endl;
-		std::cout << "Testcase directory: " << testcase_path << std::endl;
+		std::cout << "Testcase directory: " << *testcase_path << std::endl;
 	}
 
 	return 0;
