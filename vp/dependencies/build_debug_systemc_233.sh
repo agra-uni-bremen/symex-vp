3f4110d53392e36af33e949136357b36b873c23b
Sören Tempel <tempel@uni-bremen.de>
2021-02-04 18:50:37 +0100

    build_debug_systemc_233.sh: apply simulation context patches

--- vp/dependencies/build_debug_systemc_233.sh
+++ vp/dependencies/build_debug_systemc_233.sh
@@ -14,11 +14,17 @@ fi
 ##../configure CXX=clang++ CC=clang ...
 
 tar xzf $source
+cd systemc-$version
+for patch in ../systemc-patches/*.patch; do
+	echo "Applying patch ${patch##*/}..."
+	patch -p1 -i "${patch}"
+done
+
 # inject custom main function as well as thread and method stubs to avoid catching exceptions
 cp debug-patches-systemc-2.3.3/sc_main_main.cpp systemc-$version/src/sysc/kernel/
 cp debug-patches-systemc-2.3.3/sc_thread_process.cpp systemc-$version/src/sysc/kernel/
 cp debug-patches-systemc-2.3.3/sc_method_process.h systemc-$version/src/sysc/kernel/
-cd systemc-$version
+
 mkdir build && cd build
 ../configure CXXFLAGS='-std=c++17' --prefix=$PREFIX --enable-debug --with-arch-suffix=
 make -j8
