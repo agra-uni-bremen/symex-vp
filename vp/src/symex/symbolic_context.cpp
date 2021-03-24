205bf8712d7f13fde4019603d68ec8b575f0bd5b
Sören Tempel <tempel@uni-bremen.de>
2021-03-24 22:30:55 +0100

    Allow timeout configuration via SYMEX_TIMEOUT environment variable
    Requires clover update.

--- vp/src/symex/symbolic_context.cpp
+++ vp/src/symex/symbolic_context.cpp
@@ -1,4 +1,8 @@
+#include <stdlib.h>
+
 #include "symbolic_context.h"
+
+#define TIMEOUT_ENV "SYMEX_TIMEOUT"
 
 // We need to pass the SymbolicContext which includes the solver,
 // tracer, … to the sc_main method somehow. This cannot be done using
@@ -9,5 +13,10 @@ SymbolicContext symbolic_context = SymbolicContext();
 SymbolicContext::SymbolicContext(void)
 	: solver(), trace(solver), ctx(solver)
 {
-	return;
+	char *tm;
+
+	if ((tm = getenv(TIMEOUT_ENV))) {
+		auto timeout = klee::time::Span(tm);
+		solver.setTimeout(timeout);
+	}
 }
